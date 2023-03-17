cb697de3225620a7afcca59ddafe31fed091c7fc
keyurchotaliya <keyurkumar.chotaliya@neosoftmail.com>
2023-03-17 14:30:03 +0530

    chat application ready, pnding googgle components

---
+++ src/test/java/com/example/chatonelive/ChatoneLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatonelive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatoneLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
